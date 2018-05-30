1928cf368c22aee183151c4d1933d65801ea0086
Justine Bricknell <justinegayton@gmail.com>
2018-05-30 19:53:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,15 @@ function divide (a, b) {
 }
 
 function inc (n) {
-  return (n ++)
+  return (n +=)
 }
 
 function  dec(n) {
-  n--;
+  n-=;
 }
 
 function makeInt(n) {
   return parseInt(n);
 }
+
+
